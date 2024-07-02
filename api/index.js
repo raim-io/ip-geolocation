f1a3669f0146f48481fc170321f2417413921c94
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-07-03 00:01:13 +0100

    refactor: improve respone to "/" route

--- api/index.js
+++ api/index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.send("<h1>Welcome to HNG 11 - Backend Track (Task 1)</h1>");
+  res.send(`
+    <h1>Welcome to HNG 11 - Backend Track (Task 1)</h1>
+
+    <p><b>Slack Name: </b> Oluwatobiloba Raheem</p>
+  `);
 });
 
 app.get("/api/hello", async (req, res) => {
